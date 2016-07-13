da48c4a48245a1b97193613cdc5df8b5b4bc72ea
TalkWithKeyboard <610347922@qq.com>
2016-07-13 21:20:26 +0800

    1.修正项目的包依赖 2.添加数据库连接 3.创建后台操作接口类（未完成）

---
+++ Database_course_design/Database_course_design/Models/DBModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace Database_course_design.Models
+{
+    public class DBModel
+    {
+        /// <summary>
+        /// 创建主码ID
+        /// 输入：表名
+        /// 输出：该表的主码ID
+        /// 待测试
+        /// </summary>
+        public string createNewId(string tableName)
+        {
+            string newId = tableName + '_';
+            string timeStr = DateTime.Now.Ticks.ToString();
+            string time = timeStr.Substring(timeStr.Length - 10, 10);
+            newId += time;
+            return newId;
+        }
+
+        /// <summary>
+        /// 添加新用户到数据库
+        /// </summary>
+        public bool addUserInfo(string UserAccount,string UserKey,string UserName,string UserDepartment,string UserEmail,short UserIdentity,short UserGrade)
+        {
+            string newId = createNewId("USERTABLE");
+
+            USERTABLE newUser = new USERTABLE
+            {
+                USER_ID = newId,
+                USER_NAME = UserName,
+                PASSWORD = UserKey,
+                DEPARTMENT = UserDepartment,
+                EMAIL = UserEmail,
+                IDENTITY = UserIdentity,
+                GRADE = UserGrade
+            };
+
+            using (KUXIANGEntities db = new KUXIANGEntities())
+            {
+                try
+                {
+                    db.USERTABLEs.Add(newUser);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("用户添加异常");
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Database_course_design/Database_course_design/Models/OrclDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Database_course_design.Models
+{
+    public class OrclDBContext : DbContext
+    {
+        public OrclDBContext()
+            :base("OracleDbContext")
+        {
+
+        }
+        public DbSet<KUXIANGEntities> entities { get; set; }
+    }
+}
