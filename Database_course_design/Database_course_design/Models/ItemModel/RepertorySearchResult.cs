7e52483c4ada5a7f5c066f6ab6741dffb891fc42
TalkWithKeyboard <610347922@qq.com>
2016-07-15 18:35:32 +0800

    1.创建3个重新封装的类 （前段可以直接使用）

---
+++ Database_course_design/Database_course_design/Models/ItemModel/RepertorySearchResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Database_course_design.Models.ItemModel
+{
+    /// <summary>
+    /// 搜索仓库信息结果类
+    /// 仓库名
+    /// 仓库链接
+    /// 仓库信息
+    /// 仓库最近更新时间
+    /// 仓库star数量
+    /// 仓库fork数量
+    /// 仓库创建者
+    /// 仓库的一级标签
+    /// 仓库的二级标签
+    /// 仓库的三级标签
+    /// </summary>
+    public class RepertorySearchResult
+    {
+        public string RepertoryName;
+        public string RepertoryUrl;
+        public string RepertoryInfo;
+        public string RepertoryUpdateTime;
+        public int RepertoryStar;
+        public int RepertoryFork;
+        public string RepertoryCreater;
+        public string RepertoryLabel1;
+        public string RepertoryLabel2;
+        public string RepertoryLabel3;
+    }
+}
