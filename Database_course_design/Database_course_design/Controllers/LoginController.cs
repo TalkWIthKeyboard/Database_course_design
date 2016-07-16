5857828e51667503f485828912e3eeb9fe8bd145
Stanforxc <952947400@qq.com>
2016-07-15 20:03:04 -0700

    Add files via upload
    尝试登陆成功

--- Database_course_design/Database_course_design/Controllers/LoginController.cs
+++ Database_course_design/Database_course_design/Controllers/LoginController.cs
@@ -5,13 +5,25 @@ using System.Web;
 using System.Web.Mvc;
 using Database_course_design.Models;
 
-namespace Database_course_design.Controllers
+namespace WebApplication1.Controllers
 {
     public class LoginController : Controller
     {
+        private DBModel dbmodel = new DBModel();
+
         public ActionResult Login()
         {
             return View();
         }
+
+        public void Save()
+        {
+            string username = Request["email"];
+            string passwd = Request["password"];
+            if(dbmodel.sureUserLoad(username,passwd))
+                Response.Redirect("/Home/Index");
+            return;
+        }
+
     }
 }
