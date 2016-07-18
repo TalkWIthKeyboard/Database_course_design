ad49f7673f1dbc992349e4ba681f71d47d6454d1
stanforxc <13661785732@163.com>
2016-07-18 17:14:08 +0800

    test

---
+++ Database_course_design/Database_course_design/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Database_course_design.Controllers
+{
+    public class TestController :Controller
+    {
+    
+        public string SimpleMethod()
+        {
+            return "Fuck";
+        }
+
+    }
+}
